5914960ae8adc30f74ec6c85002a3436b447d162
Jacky Tang <jackytck@gmail.com>
2018-05-22 22:59:45 +0800

    :construction: temp conversions between F, C and K

--- tempunitconvert/src/update.js
+++ tempunitconvert/src/update.js
@@ -61,4 +61,37 @@ function update (msg, model) {
   return model
 }
 
+function FtoC (temp) {
+  return 5 / 9 * (temp - 32)
+}
+
+function CtoF (temp) {
+  return 9 / 5 * temp + 32
+}
+
+function KtoC (temp) {
+  return temp - 273.15
+}
+
+function CtoK (temp) {
+  return temp + 273.15
+}
+
+const FtoK = R.pipe(FtoC, CtoK)
+const KtoF = R.pipe(KtoC, CtoF)
+
+const UnitConversions = {
+  Celsius: {
+    Fahrenheit: CtoF,
+    Kelvin: CtoK
+  },
+  Fahrenheit: {
+    Celsius: FtoC,
+    Kelvin: FtoK
+  },
+  Kelvin: {
+    Celsius: KtoC,
+    Fahrenheit: KtoF
+  }
+}
 export default update
